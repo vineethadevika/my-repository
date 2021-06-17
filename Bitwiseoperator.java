43f7d16dd356f3668a027214814c42c1c3151e7f
vineethadevika <84488013+vineethadevika@users.noreply.github.com>
2021-06-17 07:32:04 +0530

    Add files via upload

---
+++ Bitwiseoperator.java
@@ -0,0 +1,9 @@
+package Pack1;
+public class ins {
+	public static void main(String[] args) {		
+		int a=5;
+		int b=7;
+		System.out.println(a | b); // 7
+		System.out.println(a & b); // 5
+		System.out.println(a ^ b); // 2
+	}}
