b03ef637623d892a1a242f886ebdbd2e3d4651dd
vineethadevika <84488013+vineethadevika@users.noreply.github.com>
2021-06-17 07:30:05 +0530

    Add files via upload

---
+++ Right shift operator.java
@@ -0,0 +1,7 @@
+package Pack1;
+public class ins {
+	public static void main(String[] args) {		
+		System.out.println(10>>2); // 10/2^2 = 10/4 = 2
+		System.out.println(20>>2); // 20/2^2 = 20/4 = 5
+		System.out.println(20>>>2); // 20/2^2 = 20/4 = 5
+	}}
