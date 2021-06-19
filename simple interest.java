386292ab032d0f8e4c1aebe56a49551afeec0166
vineethadevika <84488013+vineethadevika@users.noreply.github.com>
2021-06-19 09:45:25 +0530

    Add files via upload

---
+++ simple interest.java
@@ -0,0 +1,10 @@
+package Pack1;
+public class SI {
+	public static void main(String[] args) {
+		int p = 12000;
+		int r = 3;
+		int n = 2;
+		int si = (p*n*r)/100;
+		System.out.println("Simple Interest = "+si);
+	}
+}
