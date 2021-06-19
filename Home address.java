756fb5ee77dc3cdc826aaf04c713c670e723e84f
vineethadevika <84488013+vineethadevika@users.noreply.github.com>
2021-06-19 09:45:53 +0530

    Add files via upload

---
+++ Home address.java
@@ -0,0 +1,15 @@
+package Pack1;
+public class Home_Addr {
+	public static void main(String[] args) {
+		String name = "vineetha";
+		String dno = "2-12/1";
+		String city = "Tossipudi";
+		String state = "ap";
+		System.out.println("Address :");
+		System.out.println("---------");
+		System.out.println("Name = "+name);
+		System.out.println("DNO = "+dno);
+		System.out.println("City = "+city);
+		System.out.println("State = "+state);
+	}
+}
