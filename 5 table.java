fc8e2f156c5c2d464a5b2f9d28be3b64e0d10efb
vineethadevika <84488013+vineethadevika@users.noreply.github.com>
2021-06-19 09:46:57 +0530

    Add files via upload

---
+++ 5 table.java
@@ -0,0 +1,11 @@
+package cond;
+public class For1 {
+	public static void main(String[] args) {
+		int n = 5, i=1;
+		while(i<=10) 
+		{
+			 System.out.println(n + " X "  + i +" = "+ (n*i));
+			 i++; 
+		}
+		System.out.println("end of loop");
+	} }
